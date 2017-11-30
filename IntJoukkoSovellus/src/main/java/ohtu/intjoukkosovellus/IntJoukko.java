f64131fc9fbaafd1958a190109946f3d0304b67e
JL <lab.galom@outlook.com>
2017-11-30 16:04:16 +0200

    viikko5 teht 2 Tennis

--- IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -9,26 +9,33 @@ public class IntJoukko {
     private int[] ljono;      // Joukon luvut säilytetään taulukon alkupäässä. 
     private int alkioidenLkm;    // Tyhjässä joukossa alkioiden_määrä on nolla. 
 
-    public IntJoukko(int... parametrit) {
-        if (parametrit.length > 0) {
-            if (parametrit[0] > 0) {
-                ljono = new int[parametrit[0]];
-            } else {
-                ljono = new int[KAPASITEETTI];
-            }
-            if (parametrit[1] > 0) {
-                this.kasvatuskoko = kasvatuskoko;
-            } else {
-                this.kasvatuskoko = OLETUSKASVATUS;
-            }
-        } else {
-            ljono = new int[KAPASITEETTI];
-
-            this.kasvatuskoko = OLETUSKASVATUS;
-        }
+    public IntJoukko() {
+        ljono = new int[KAPASITEETTI];
         alustaLjono();
+        this.kasvatuskoko = OLETUSKASVATUS;
     }
 
+    public IntJoukko(int kapasiteetti, int kasvatuskoko) {
+
+        if (kapasiteetti < 0) {
+            throw new IndexOutOfBoundsException("Kapasitteetti väärin");//heitin vaan jotain :D
+        }
+        if (kasvatuskoko < 0) {
+            throw new IndexOutOfBoundsException("kapasiteetti2");//heitin vaan jotain :D
+        }
+
+        this.kasvatuskoko = kasvatuskoko;
+        ljono = new int[KAPASITEETTI];
+        alustaLjono();
+
+    }
+
+    public IntJoukko(int kapasiteetti) {
+        this(kapasiteetti, OLETUSKASVATUS);
+
+    }
+
+//   
     private void alustaLjono() {
         for (int i = 0; i < ljono.length; i++) {
             ljono[i] = 0;
@@ -72,14 +79,18 @@ public class IntJoukko {
             if (luku == ljono[i]) {
                 kohta = i; //siis luku löytyy tuosta kohdasta :D
                 ljono[kohta] = 0;
-                korjaaIndeksitJonosta(kohta);
-                alkioidenLkm--;
-
+                laskeUusiArvo(kohta);
                 return true;
 
             }
         }
         return false;
+    }
+    private void laskeUusiArvo(int kohta){
+        int tamaKohta = kohta;
+        ljono[tamaKohta] = 0;
+        korjaaIndeksitJonosta(kohta);
+        
     }
 
     private void korjaaIndeksitJonosta(int kohta) {
@@ -89,6 +100,7 @@ public class IntJoukko {
             ljono[j] = ljono[j + 1];
             ljono[j + 1] = apu;
         }
+        alkioidenLkm--;
     }
 
     private void kopioiTaulukko(int[] vanha, int[] uusi) {
@@ -125,6 +137,20 @@ public class IntJoukko {
         return taulu;
     }
 
+    public static IntJoukko yhdiste2(IntJoukko a, IntJoukko b) {
+        IntJoukko x = new IntJoukko();
+
+        for (int alkioAssa : a.toIntArray()) {
+            x.lisaa(alkioAssa);
+        }
+
+        for (int alkioBssa : b.toIntArray()) {
+            x.lisaa(alkioBssa);
+        }
+
+        return x;
+    }
+
     public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
         IntJoukko x = new IntJoukko();
         int[] aTaulu = a.toIntArray();
@@ -139,13 +165,12 @@ public class IntJoukko {
     public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
         IntJoukko y = new IntJoukko();
         int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
         for (int i = 0; i < aTaulu.length; i++) {
-            for (int j = 0; j < bTaulu.length; j++) {
-                if (aTaulu[i] == bTaulu[j]) {
-                    y.lisaa(bTaulu[j]);
-                }
+            int lukuAssa = aTaulu[i];
+            if (b.kuuluu(lukuAssa)) {
+                y.lisaa(lukuAssa);
             }
+
         }
         return y;
 
@@ -154,7 +179,6 @@ public class IntJoukko {
     public static IntJoukko erotus(IntJoukko a, IntJoukko b) {
         IntJoukko z = new IntJoukko();
         int[] aTaulu = a.toIntArray();
-        int[] bTaulu = b.toIntArray();
         for (int i = 0; i < aTaulu.length; i++) {
             z.lisaa(aTaulu[i]);
             z.poista(i);
